4203551690b6d6da260b45859801e84a84f8d003
Joshua Paling <joshua.paling@gmail.com>
2013-09-17 20:51:24 +1000

    basic 'hello from backbone' working.
    Signed-off-by: Joshua Paling <joshua.paling@gmail.com>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require jquery
 //= require underscore
 //= require backbone
-//= require quick_blog
+//= require joshua_paling
 //= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
 //= require_tree ./views
 //= require_tree ./routers
-//= require_tree .
