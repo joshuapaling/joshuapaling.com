0e21d864756574e7711425730f1fdd81379d7356
Joshua Paling <joshua.paling@gmail.com>
2014-01-03 21:58:44 +1100

    install foundation for admin area
    Signed-off-by: Joshua Paling <joshua.paling@gmail.com>

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -11,12 +11,4 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
-//= require underscore
-//= require backbone
-//= require joshua_paling
-//= require_tree ../templates
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ./views
-//= require_tree ./routers
 $(function(){ $(document).foundation(); });
