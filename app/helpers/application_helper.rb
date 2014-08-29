761c5405246af342a0e06274fbbee90940c5d419
Joshua Paling <joshua.paling@gmail.com>
2014-08-29 21:37:17 +1000

    add flash key classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+
+  # from https://coderwall.com/p/jzofog
+  def flash_class(level)
+      case level.to_sym
+          when :notice then "alert alert-info"
+          when :success then "alert alert-success"
+          when :error then "alert alert-danger"
+          when :alert then "alert alert-warning"
+      end
+  end
+
 end
